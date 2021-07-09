92a52351a10a3031c388cf46d49f30a1afd0f2ea
Zura <70130285+zura-papiashvili@users.noreply.github.com>
2021-07-09 09:48:50 +0400

    jwt implementation

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,19 +1,20 @@
 import { Controller, Get, Post, UseGuards, Request } from '@nestjs/common';
-import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly authService: AuthService) {}
 
   @UseGuards(LocalAuthGuard)
   @Post('login')
   login(@Request() req): any {
-    return req.user;
+    return this.authService.login(req.user);
   }
-
+  @UseGuards(JwtAuthGuard)
   @Get('protected')
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(@Request() req): string {
+    return req.user;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import * as passport from 'passport';
+import * as session from 'express-session';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   await app.listen(3000);
 }
 bootstrap();
